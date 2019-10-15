83d9d44681fd42604ea1cb7a651a86e445e7e3f9
takiryo <ec2-user@ip-172-31-20-235.us-east-2.compute.internal>
2019-10-15 16:47:09 +0000

    HTMLを記入

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta chaset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews create</title>
+    </head>
+    <body>
+        <h1>Myニュースを作成する</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta chaset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews edit</title>
+    </head>
+    <body>
+        <h1>Myニュースを編集する</h1>
+    </body>
+</html>
